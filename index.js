8430d352687ecab2e82500c0b68e0bfccbf39d2d
Unzor <71789103+Unzor@users.noreply.github.com>
2021-11-22 08:32:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Modulin = {loadModule: function(module){
     return new Promise((resolve, reject) => {
-    fetch(`https://modulin.herokuapp.com/module/uniq`)
+    fetch(`https://modulin.herokuapp.com/module/` + module)
     .then(response => response.text())
     .then(response => {
 eval(response);
